6ee48bcc6828c7786beb892b87d3145dc758f058
Eric Demko <edemko@layer3com.com>
2021-03-26 13:03:03 -0400

    fixed python/is2; something's wrong with lol/loless

---
+++ stdlib/lol/loless/check.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -e
+
+test -x "$(command -v loless 2>/dev/null)" >/dev/null 2>&1
--- stdlib/python/is2/check.sh
+++ stdlib/python/is2/check.sh
@@ -4,4 +4,5 @@ set -e
 exec >/dev/null 2>/dev/null
 
 test -x "$(command -v python 2>/dev/null)"
-python --version | grep -q '^Python 2\.'
+# apparently python 2.7.12 puts the result of -V onto stderr‽
+python --version 2>&1 | grep -q '^Python 2\.'
